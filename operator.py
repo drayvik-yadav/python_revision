d4fa478b58b0613b7681c6d6ebdd5ba60cab2542
abhishek <ak3221110@gmail>
2026-03-05 23:21:55 +0530

    1st part operator

---
+++ operator.py
@@ -0,0 +1,21 @@
+#arithmetic Operator + - * / % // **
+#Addition(+),Subtraction(-),Multiplication(*),Division(/)
+a=5
+b=2
+A=a+b
+S=a-b
+M=a*b
+D=a/b
+print(A,type(A))
+print(S,type(S))
+print(M,type(M))
+print(D,type(D))
+print(type(a))
+
+#flore division(//), Modulo(%),Exponentiation(**)
+F=a//b #2
+R=a%b #1 remender
+P=a**b #25 b to the power of a 
+print(F,type(F))
+print(R,type(R))
+print(P,type(P))
