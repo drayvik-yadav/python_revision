efa44320c1a1500d6c58dac934df651c9d0d0482
Abhishek Kumar <152717508+drayvik-yadav@users.noreply.github.com>
2026-04-05 03:28:18 +0000

    half piramide

--- star_printing.py
+++ star_printing.py
@@ -1,5 +1,15 @@
 n=5
+#half piramide
 for i in range(n+1):
     print("*"*i)
 for i in range(n+1,0,-1):
     print("*"*i)
+
+#half piramide
+re=1#repet
+for i in range(n):
+    
+    spece=n-i
+    le_re=re-1
+    print(" "*spece, '*'*re,'*'*le_re,sep="")
+    re+=1
